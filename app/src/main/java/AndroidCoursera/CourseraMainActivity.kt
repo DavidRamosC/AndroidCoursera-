f84403c0548c915adbdf98b7fbd0cf24f5f78192
DavidRamosC <david.ramos-c@uniminuto.edu.co>
2023-09-11 23:06:57 -0500

    Initial commit

---
+++ app/src/main/java/AndroidCoursera/CourseraMainActivity.kt
@@ -0,0 +1,13 @@
+package AndroidCoursera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidcoursera.R
+
+class CourseraMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coursera_main)
+
+    }
+}
